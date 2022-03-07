8f1cfd6da1d635d90f2689b249ae4f92ebfb4881
Azryel <AzryelShroud@gmail.com>
2022-03-07 17:21:50 +0100

    stats preview component

---
+++ stats_preview_component/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "very-dark-blue": "hsl(233, 47%, 7%)",
+        "darkDesaturatedBlue": "hsl(244, 38%, 16%)",
+        "softViolet": "hsl(277, 64%, 61%)",
+        "mainPara": "hsla(0, 0%, 100%, 0.75)",
+        "stats": "hsla(0, 0%, 100%, 0.6)"
+      },
+      fontFamily: {
+        "inter": ["Inter"],
+        "lexend": ["Lexend+Deca"]
+      }
+    },
+  },
+  plugins: [],
+}
